f401df9af2887cdaa0e0aab92135202d091e5937
Chekonjak <si3ilante@gmail.com>
2018-02-21 14:32:53 -0800

    Adding MyArrayList exercise for generics practice and better understanding of ArrayList

---
+++ List Example/src/MyArrayList.java
@@ -0,0 +1,256 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * An implementation of MyList with an array (a longer exercise would be to
+ * implement the List interface as is done in the class java.util.ArrayList: the
+ * source of the ArrayList class is available from Sun. Check it out).
+ */
+
+public class MyArrayList<E> implements MyList<E> {
+
+    // Use an array for the implementation
+    private E[] items;
+
+    // Default capacity of the array
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // Number of elements in the array
+    private int size;
+
+    /**
+     * Constructs a MyArrayList with a specified capacity
+     */
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("capacity = " + initialCapacity);
+        }
+        items = (E[]) new Object[initialCapacity];
+    }
+
+    /**
+     * Constructs a MyArrayList with a default capacity
+     */
+    public MyArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns true if this list contains no elements.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Appends the specified element to the end of this list
+     */
+    public boolean add(E o) {
+        // If there is no room in the array items
+        // Make room for the new element
+        if (size == items.length) {
+            // E[] temp = (E[]) new Object[items.length + DEFAULT_CAPACITY];
+            // // make the copy by hand
+            // for(int i = 0; i < items.length; i ++) {
+            // temp[i] = items[i];
+            // }
+            // items = temp;
+            // or use a library method of java
+            // one example
+            items = Arrays.copyOf(items, items.length + DEFAULT_CAPACITY);
+            // other example
+            // System.arraycopy(items, 0, temp, 0, items.length);
+            // items = temp;
+
+        }
+        // add the new element
+        items[size] = o;
+        size++;
+        return true;
+    }
+
+    /**
+     * Empties this List
+     */
+    public void clear() {
+        size = 0; //
+        // running GC is optional
+        items = (E[]) new Object[DEFAULT_CAPACITY]; // TODO: OH MY GOD IT'S GARBAGE COLLECTION!!!
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        return items[index];
+    }
+
+    /**
+     * Returns the index of the specified element (-1 if there is no match)
+     */
+    public int indexOf(Object o) {
+        // If o is null (look for a null element in the array)
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (o == items[i]) {
+                    return i;
+                }
+            }
+        } else // o is an object (use equals)
+        {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(items[i])) {
+                    return i;
+                }
+            }
+        }
+
+        // If we get here, o is not in the list
+        return -1;
+    }
+
+    /**
+     * Returns true if this list contains the specified element.
+     */
+    public boolean contains(Object o) {
+        // easy with indexOf
+        return indexOf(o) != -1;
+    }
+
+    /**
+     * Removes the element in the List at position index
+     */
+    public boolean remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        for (int i = index; i <= size - 2; i++) {
+            items[i] = items[i + 1];
+        }
+        size--;
+        return true;
+    }
+
+    /**
+     * Removes the element in the List at position index
+     */
+    public boolean remove(Object o) {
+        // easy with indexOf and remove
+        int index = indexOf(o);
+        return index != -1 ? remove(index) : false;
+    }
+
+    /**
+     * Adds the specified object at the specified location
+     */
+    public boolean add(int index, E o) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        // one way: add at the end and then shift the elements around
+        add(o); // to use the code that makes the array bigger if needed
+        for (int i = size - 1; i > index; i--) {
+            items[i] = items[i - 1];
+        }
+
+        items[index] = o;
+        return true;
+    }
+
+    /**
+     * Is this List equal to the specified object?
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MyArrayList) {
+            // o is an ArrayList
+            MyArrayList<E> list = (MyArrayList<E>) o;
+            // if the number of elements is not the same, this and o are not the
+            // same
+            if (list.size != size) {
+                return false;
+            }
+
+            /*
+             * boolean b = true; for (int i = 0; i < size && b; i ++) { if
+             * (items[i] == null) { b &= (list.items[i] == null); } else { b &=
+             * (items[i].equals(list.items[i])); } } return b;
+             */
+
+            // Check the elements one by one
+            for (int i = 0; i < size; i++) {
+                if (items[i] == null) {
+                    if (list.items[i] != null) {
+                        return false;
+                    }
+                } else if (!items[i].equals(list.items[i])) {
+                    return false;
+                }
+            }
+            // At this point, the lists are equal
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * An inner class to define the iterator
+     */
+    private class MyIterator implements Iterator<E> {
+
+        // index of the next element to visit
+        private int index = 0;
+
+        private boolean nextCalled;
+
+        /**
+         * Any element left in the list?
+         */
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        /**
+         * Returns the current element in the list and move to the next element
+         */
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            nextCalled = true;
+            index ++;
+            return items[index - 1];
+        }
+
+        /**
+         * Removes the last object returned by next
+         */
+        public void remove() {
+            if (!nextCalled) {
+                throw new IllegalStateException();
+            }
+            MyArrayList.this.remove(index - 1);
+        }
+    }
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * @return an iterator over the elements in this list in proper sequence.
+     */
+    public Iterator<E> iterator() {
+        return new MyIterator();
+    }
+}
---
+++ List Example/src/MyLinkedList.java
@@ -0,0 +1,309 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import javax.management.InstanceNotFoundException;
+
+/**
+ * An implementation of MyList with a linked list (a longer exercise would be to
+ * implement the List interface as is done in the class java.util.LinkedList:
+ * the source of the LinkedList class is available from Sun. Check it out).
+ */
+
+public class MyLinkedList<E> implements MyList<E> {
+
+    // A private class to represent a Node in the linked list
+    private class Node {
+        public E item;
+
+        public Node next;
+
+        // a convenient constructor
+        public Node(E o) {
+            this.item = o;
+            this.next = null;
+        }
+    }
+
+    // The start of the linked list
+    private Node head;
+
+    // The last Node in the linked list
+    private Node tail;
+
+    // Number of elements in the list
+    private int size;
+
+    /**
+     * Creates an empty list (this constructor is not necessary)
+     */
+    public MyLinkedList() {
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     */
+    public int size() {
+        // without a size field
+        // int count = 0;
+        // for (Node p = head; p != null; p = p.next) {
+        // count ++;
+        // }
+        // return count;
+        // with a size field -> more efficient
+        return size;
+    }
+
+    /**
+     * Returns true if this list contains no elements.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Appends the specified element to the end of this list
+     */
+    public boolean add(E o) {
+        Node n = new Node(o);
+        // If this is the first element in the list
+        if (isEmpty()) {
+            head = n;
+        } else {
+            // If the list is not empty, use tail
+            tail.next = n;
+        }
+        // update tail
+        tail = n;
+        // update size
+        size++;
+        return true;
+    }
+
+    /**
+     * Empties this List
+     */
+    public void clear() {
+        // update head, tail and size
+        size = 0;
+        head = tail = null;
+    }
+
+    /**
+     * Returns the element at the specified position in this list.
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        // Find it
+        Node p = head;
+        for (int i = 1; i <= index; i++) {
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    /**
+     * Returns the index of the specified element (-1 if there is no match)
+     */
+    public int indexOf(Object o) {
+        Node p = head;
+        for (int i = 0; i < size; i++) {
+            // If o is null
+            if (o == null) // look for a null element in the list
+            {
+                if (p.item == null) {
+                    return i;
+                }
+            } else {// o is an object (use equals)
+                if (o.equals(p.item)) {
+                    return i;
+                }
+            }
+            p = p.next;
+        }
+        // if we get here, o is not in the list
+        return -1;
+    }
+
+    /**
+     * Returns true if this list contains the specified element.
+     */
+    public boolean contains(Object o) {
+        // easy with indexOf
+        int index = indexOf(o);
+        return index != -1;
+    }
+
+    /**
+     * Removes the element in the List at position index
+     */
+    public boolean remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        // Find the corresponding node (prev points to the node before p)
+        Node prev = null, p = head;
+        for (int i = 1; i <= index; i++) {
+            prev = p;
+            p = p.next;
+        }
+        // Remove it
+        // Special case for the first node
+        if (index == 0) {
+            head = head.next;
+        } else {
+            prev.next = p.next;
+        }
+        // If the last node has been removed, update tail
+        if (p == tail) {
+            tail = prev;
+        }
+        // update size
+        size--;
+        return true;
+    }
+
+    /**
+     * Removes the element in the List at position index
+     */
+    public boolean remove(Object o) {
+        // easy with indexOf and remove (but expensive)
+        int index = indexOf(o);
+        if (index != -1) {
+            return remove(index);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Adds the specified object at the specified location
+     */
+    public boolean add(int index, E o) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+        // Find the corresponding node
+        Node prev = null, p = head;
+        for (int i = 1; i <= index; i++) {
+            prev = p;
+            p = p.next;
+        }
+        // Add a node between prev and p
+        Node n = new Node(o);
+        n.next = p;
+        if (prev != null) {
+            prev.next = n;
+        } else {
+            // Special case (first node)
+            head = n;
+        }
+        // Update tail if necessary
+        if (index == size) {
+            tail = n;
+        }
+        // Update size
+        size++;
+        return true;
+    }
+
+    /**
+     * Is this List equal to the specified object?
+     */
+    public boolean equals(Object o) {
+        if (o instanceof MyLinkedList) {
+            // o is a MyLinkedList
+            MyLinkedList<E> list = (MyLinkedList<E>) o;
+            // if the number of elements is not the same, this and o are not the
+            // same
+            if (list.size != this.size) {
+                return false;
+            }
+            // Check the elements one by one
+            for (Node p = head, q = list.head; p != null; p = p.next, q = q.next) {
+                if (p.item == null) { // why not just if (p.item == null &&
+                    // q.item != null) { return false;}
+                    if (q.item != null) {
+                        return false;
+                    }
+                } else if (!p.item.equals(q.item)) {
+                    return false;
+                }
+            }
+            // At this point, the lists are equal
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * An inner class to define the iterator
+     */
+    private class MyIterator implements Iterator<E> {
+        Node p = head, prev = null, prevprev = null;
+        boolean nextCalled;
+
+        /**
+         * Any element left in the list?
+         */
+        public boolean hasNext() {
+            return p != null;
+        }
+
+        /**
+         * Return the current element in the list and move to the next element
+         */
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            nextCalled = true;
+            E item = p.item;
+            prevprev = prev;
+            prev = p;
+            p = p.next;
+            return item;
+        }
+
+        /**
+         * Remove the object currently pointed at by the iterator
+         */
+        public void remove() {
+            if (!nextCalled) {
+                throw new IllegalStateException();
+            }
+            nextCalled = false;
+            // remove the node pointed at by prev
+            if (prevprev != null) {
+                prevprev.next = p;
+                prev = prevprev;
+            } else {
+                head = p;
+                prev = null;
+            }
+            if (p == null) {
+                // we just removed the last one
+                tail = prevprev;
+            }
+            prevprev = null;
+            size --;
+
+        }
+    }
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     *
+     * @return an iterator over the elements in this list in proper sequence.
+     */
+    public Iterator<E> iterator() {
+        return new MyIterator();
+    }
+}
+
+
+
+
+
